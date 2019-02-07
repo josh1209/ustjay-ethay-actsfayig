217752ca945d92f71a22d4a955ca8474e60b6acf
Josh Hoff <jhoff1985@hotmail.com>
2019-02-06 21:55:43 -0800

    Finished Pig Latin assignment

--- main.py
+++ main.py
@@ -14,12 +14,22 @@ def get_fact():
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText()[7:-1]
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    form = {'input_text': fact}
+    req = requests.post('https://hidden-journey-62459.herokuapp.com/piglatinize/', form, allow_redirects=False)
+    link = req.headers['Location']
+
+    #gets the text at the pig latin link
+    req2 = requests.get(link)
+    #gets the quote from the long string of text
+    page = req2.text.split('\n')[8].strip()
+
+    return link
 
 
 if __name__ == "__main__":
